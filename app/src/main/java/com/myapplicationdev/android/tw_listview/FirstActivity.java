9af9d710cfd455cea2e60736542ba792798f84d8
weirdo2809 <15035648@myrp.edu.sg>
2017-04-27 12:51:01 +0800

    First Part done

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,9 +1,13 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,14 +21,28 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        lv = (ListView) findViewById(R.id.lvYear);
 
         al = new ArrayList<String>();
         al.add("Year 1");
+        al.add("Year 2");
+        al.add("Year 3");
 
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
+        lv.setAdapter(aa);
 
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedYear = al.get(position);
 
+                Toast.makeText(FirstActivity.this, selectedYear,
+                        Toast.LENGTH_LONG).show();
 
-
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("year", selectedYear);
+                startActivity(intent);
+            }
+        });
     }
 }
